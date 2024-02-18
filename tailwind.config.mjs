625552157f7695ea35512953f816675219f48c51
Frank Stallone <frank@designzen.co>
2024-02-18 08:15:11 -0500

    refactor: ♻️  Using TW for all the things

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -27,7 +27,6 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   // Add color classes to safe list so they are always generated
   safelist: [],
-  presets: [],
   theme: {
     screens: {
       sm: `${viewportTokens.min}px`,
