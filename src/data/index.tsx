0073074f52aa8ee158b6624347988b0a49b9da3b
Frank Stallone <frank@designzen.co>
2024-05-26 17:54:30 -0400

    feat: ✨ Adding Inspirational Examples

--- src/data/index.tsx
+++ src/data/index.tsx
@@ -6,6 +6,7 @@ export const options: PriceCardProps[] = [
       title: 'Professional Photography of Products',
       description:
         'High-quality images showcasing your products in a professional, light, airy, and minimalistic style.',
+      exampleId: 'PhotographyExamples',
       type: 'Photo',
       packages: [
         {
@@ -40,6 +41,7 @@ export const options: PriceCardProps[] = [
       title: 'Short Form Product Videos',
       description:
         'Engaging short videos tailored for Instagram & TikTok, designed to captivate and inspire your audience through a feeling of lux serenity. These can be product showcases, unboxings, how-tos, before-and-afters, or any other ideas youd like to see brought to life.',
+      exampleId: 'ShortFormExamples',
       type: 'UGC Video',
       packages: [
         {
@@ -74,6 +76,7 @@ export const options: PriceCardProps[] = [
       title: 'Inspiration and Vlog Videos',
       description:
         'Personalized and heartfelt video content produced in studio quality, sharing lifestyle tips, inspiration, self-care routines, and day-to-day product usage.',
+      exampleId: 'InspirationalExamples',
       type: 'UGC Video',
       packages: [
         {
