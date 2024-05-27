f965a45d37a821388e330c4871ca2a8f2a0aec67
Frank Stallone <frank@designzen.co>
2024-05-27 06:21:43 -0400

    feat: ✨ Adding GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,23 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-
-import react from "@astrojs/react";
+import partytown from '@astrojs/partytown';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://skinschema.com',
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), sitemap(), react()]
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+    react(),
+    partytown({
+      // Example: Add dataLayer.push as a forwarding-event.
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
 });
